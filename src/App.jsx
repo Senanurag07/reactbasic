a5f4f70dfc5505bc31283b8f8cf8dee11ee65ebc
Senanurag07 <senanurag061@gmail.com>
2024-09-20 13:40:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+
+// function App() {
+  
+
+//   return (
+//    <div>
+//     <h1>hello</h1>
+//    </div>
+//   )
+// }
+
+
+
+const App = () =>{
+
+  return(
+
+    <div>hello</div>
+  )
+}
+export default App
